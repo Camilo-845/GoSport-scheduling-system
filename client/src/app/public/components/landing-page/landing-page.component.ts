3efdf91dfebab84850b04702a190f55ab7303f4e
CamiloSar <camilo.sarmiento.amado@gmail.com>
2025-04-24 09:38:57 -0500

    feat(client): footer component

--- client/src/app/public/components/landing-page/landing-page.component.ts
+++ client/src/app/public/components/landing-page/landing-page.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FooterComponent } from '@/components/footer/footer.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [NavBarComponent],
+  imports: [NavBarComponent, FooterComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
