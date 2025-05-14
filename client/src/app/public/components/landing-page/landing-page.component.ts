dde2a4b2a85034a9d2cf8c2b72c1d3e197404d17
CamiloSar <camilo.sarmiento.amado@gmail.com>
2025-05-14 17:29:17 -0500

    feat(client): move component to superior container

--- client/src/app/public/components/landing-page/landing-page.component.ts
+++ client/src/app/public/components/landing-page/landing-page.component.ts
@@ -1,8 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { FooterComponent } from '@/components/footer/footer.component';
 import { SportsGridComponent } from './components/sports-grid/sports-grid.component';
 import { CtaSectionComponent } from './components/cta-section/cta-section.component';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-landing-page',
