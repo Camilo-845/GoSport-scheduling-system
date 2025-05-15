4f1cb330529e9b39310c36b30c0725238de0b2eb
CamiloSar <camilo.sarmiento.amado@gmail.com>
2025-05-15 17:37:43 -0500

    feat(client): event preview component

--- client/src/app/private/events/components/event-preview-list/event-preview-list.component.ts
+++ client/src/app/private/events/components/event-preview-list/event-preview-list.component.ts
@@ -1,13 +1,37 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+} from '@angular/core';
+import { EventListComponent } from '../event-list/event-list.component';
+import { EventService } from '../../services';
+import { Router } from '@angular/router';
+import { AppRoutes } from '@/app.routes';
 
 @Component({
   selector: 'app-event-preview-list',
   standalone: true,
-  imports: [],
+  imports: [EventListComponent],
   templateUrl: './event-preview-list.component.html',
   styleUrl: './event-preview-list.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EventPreviewListComponent {
+  eventService = inject(EventService);
+  router = inject(Router);
+  events = computed(() => {
+    const eventsMap = this.eventService.state().events;
+    return Array.from(eventsMap.values()).slice(0, 5);
+  });
 
+  constructor() {
+    this.eventService.getEvents();
+  }
+
+  goMoreEvents() {
+    this.router.navigate([
+      `${AppRoutes.private.root}/${AppRoutes.private.events}`,
+    ]);
+  }
 }
