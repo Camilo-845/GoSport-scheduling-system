402949a7f6c452d380197e1cca6e4b962b15ac2f
CamiloSar <camilo.sarmiento.amado@gmail.com>
2025-05-15 22:48:14 -0500

    feat(client): home add sports list preview

--- client/src/app/private/home/home.component.ts
+++ client/src/app/private/home/home.component.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { EventPreviewListComponent } from '../events/components';
+import { SportPreviewListComponent } from '../sports/components/sport-preview-list/sport-preview-list.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [EventPreviewListComponent, SportPreviewListComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
