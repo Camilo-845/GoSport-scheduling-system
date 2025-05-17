77ece3ee0975cc8c8e81066fef34e89322bc06c2
CamiloSar <camilo.sarmiento.amado@gmail.com>
2025-05-17 13:24:36 -0500

    fix(api): fix time validator on event

--- api/src/app/event/event.validator.ts
+++ api/src/app/event/event.validator.ts
@@ -1,7 +1,12 @@
 import { z } from "zod";
+
+const timeRegex = /^([01]\d|2[0-3]):([0-5]\d)$/;
+
 export const eventCreateSchema = z.object({
   nombre: z.string(),
   fecha: z.string().date(),
-  hora_inicio: z.string().time(),
+  hora_inicio: z
+    .string()
+    .refine((val) => timeRegex.test(val), { message: "Invalid Time Format" }),
   id_cancha: z.number(),
 });
