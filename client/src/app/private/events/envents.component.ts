660978cda68d7064e4cbe38ebeed48af27dbab06
CamiloSar <camilo.sarmiento.amado@gmail.com>
2025-05-17 14:19:49 -0500

    feat(client): events componente create Event Button

--- client/src/app/private/events/envents.component.ts
+++ client/src/app/private/events/envents.component.ts
@@ -6,6 +6,8 @@ import {
 } from '@angular/core';
 import { EventListComponent } from './components';
 import { EventService } from './services';
+import { Router } from '@angular/router';
+import { AppRoutes } from '@/app.routes';
 
 @Component({
   selector: 'app-envents',
@@ -21,8 +23,17 @@ export class EnventsComponent {
     const eventsMap = this.eventService.state().events;
     return Array.from(eventsMap.values());
   });
+  router = inject(Router);
 
   constructor() {
     this.eventService.getEvents();
+  }
+
+  onCreateEvent() {
+    this.router.navigate([
+      AppRoutes.private.root,
+      AppRoutes.private.events.root,
+      AppRoutes.private.events.create,
+    ]);
   }
 }
